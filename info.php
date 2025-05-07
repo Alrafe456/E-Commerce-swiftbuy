6c74a5b885f125ebc9284abc8a2d2b176905ec01
Alrafe456 <2222456@iub.edu.bd>
2025-05-07 11:44:50 +0600

    Update info.php

--- info.php
+++ info.php
@@ -5,12 +5,12 @@ header('Content-Type: application/json');
 
 
 $project_details = array(
-    "name" => "Your Full Name", // Replace with your name
-    "id" => "Your University ID", // Replace with your university ID
-    "personal_notion_page" => "https://link_to_your_notion_page", // Replace with your personal Notion page link
-    "personal_group_page_notion" => "https://link_to_your_group_notion_page", // Replace with your group Notion page link
-    "github_id" => "YourGitHubID", // Replace with your GitHub ID
-    "project_github_link" => "https://github.com/your_project_link" // Replace with your project's GitHub link
+    "name" => "Ahnaf Tahmid Al Rafe",
+    "id" => "2222456",
+    "personal_notion_page" => "https://www.notion.so/168a1ff4fd22484eab545c8c9b82c085",
+    "personal_group_page_notion" => "https://www.notion.so/E-commerce-1ecd0e3260468074bf8cc820527e7516",
+    "github_id" =>  "Alrafe456", 
+    "project_github_link" => "https://github.com/Alrafe456/E-Commerce-swiftbuy"
 );
 
 
